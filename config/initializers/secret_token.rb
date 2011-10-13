80f120c1a3539ee47e4f3a49168bbce58c10fd04
Brennan Falkner <Brennan@bfalkner.com>
2011-10-13 02:33:56 -0400

    Init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shannon::Application.config.secret_token = 'e2b18097cd51137f9b2c5e31895ea5b11d8a348c997ff038b4f4b1393a615419460d9b405b76ec110442a54546e547bc10a83a88f15a62f028a1ff2046d6cf9a'
