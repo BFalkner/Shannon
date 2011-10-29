fba095e7483be6aff8b71e87c1878b075da7b6b1
Brennan Falkner <Brennan@bfalkner.com>
2011-10-29 08:58:22 -0400

    Disable pagination buttons appropiately.

--- app/sproutcore/controllers/photosets_controller.js
+++ app/sproutcore/controllers/photosets_controller.js
@@ -7,14 +7,22 @@ Gallery.PhotosetsController = SC.ArrayProxy.create({
   current_page: [],
 
   next_page: function() {
-    if (this.get("page") == this.get("_last_page_index")) return;
+    if (this.get("has_no_next_page")) return;
     this.set("page", this.get("page") + 1);
   },
 
   previous_page: function() {
-    if (this.get("page") == 0) return;
+    if (this.get("has_no_previous_page")) return;
     this.set("page", this.get("page") - 1)
   },
+
+  has_no_next_page: function() {
+    return this.get("page") == this.get("_last_page_index");
+  }.property("page", "_last_page_index"),
+
+  has_no_previous_page: function() {
+    return this.get("page") == 0;
+  }.property("page"),
 
   _load_current_page: function() {
     var begin = this.get("page") * this.get("per_page");
@@ -23,8 +31,8 @@ Gallery.PhotosetsController = SC.ArrayProxy.create({
   }.observes("page", "@each"),
 
   _last_page_index: function() {
-    return Math.ceil(this.get("length") / this.get("per_page")) - 1;
-  }.property(),
+    return Math.max(0, Math.ceil(this.get("length") / this.get("per_page")) - 1);
+  }.property("@each"),
 
   select: function(photoset) {
     photoset.fetchPhotos();
