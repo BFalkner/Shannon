f281c253f09eba46499496d90e79d1731d0808b1
Brennan Falkner <Brennan@bfalkner.com>
2011-10-29 09:17:24 -0400

    Extract pagination into a mixin.

--- app/sproutcore/controllers/photosets_controller.js
+++ app/sproutcore/controllers/photosets_controller.js
@@ -1,38 +1,9 @@
+//= require ../mixins/paginates
 
-Gallery.PhotosetsController = SC.ArrayProxy.create({
+Gallery.PhotosetsController = SC.ArrayProxy.create(Gallery.Paginates, {
   content: [],
   selected: null,
-  page: 0,
   per_page: 12,
-  current_page: [],
-
-  next_page: function() {
-    if (this.get("has_no_next_page")) return;
-    this.set("page", this.get("page") + 1);
-  },
-
-  previous_page: function() {
-    if (this.get("has_no_previous_page")) return;
-    this.set("page", this.get("page") - 1)
-  },
-
-  has_no_next_page: function() {
-    return this.get("page") == this.get("_last_page_index");
-  }.property("page", "_last_page_index"),
-
-  has_no_previous_page: function() {
-    return this.get("page") == 0;
-  }.property("page"),
-
-  _load_current_page: function() {
-    var begin = this.get("page") * this.get("per_page");
-    var end = begin + this.get("per_page");
-    this.set("current_page", this.slice(begin, end));
-  }.observes("page", "@each"),
-
-  _last_page_index: function() {
-    return Math.max(0, Math.ceil(this.get("length") / this.get("per_page")) - 1);
-  }.property("@each"),
 
   select: function(photoset) {
     photoset.fetchPhotos();
---
+++ app/sproutcore/mixins/paginates.js
@@ -0,0 +1,37 @@
+Gallery.Paginates = SC.Mixin.create({
+  per_page: SC.required(),
+  page: 0,
+  current_page: [],
+
+  next_page: function() {
+    if (this.get("has_no_next_page")) return;
+    this.set("page", this.get("page") + 1);
+  },
+
+  previous_page: function() {
+    if (this.get("has_no_previous_page")) return;
+    this.set("page", this.get("page") - 1)
+  },
+
+  has_no_next_page: function() {
+    return this.get("page") == this.get("_last_page_index");
+  }.property("page", "_last_page_index"),
+
+  has_no_previous_page: function() {
+    return this.get("page") == 0;
+  }.property("page"),
+
+  _load_current_page: function() {
+    var begin = this.get("page") * this.get("per_page");
+    var end = begin + this.get("per_page");
+    this.set("current_page", this.slice(begin, end));
+  }.observes("page", "@each"),
+
+  _last_page_index: function() {
+    return Math.max(0, Math.ceil(this.get("length") / this.get("per_page")) - 1);
+  }.property("@each"),
+
+  _reset_page: function() {
+    this.set("page", 0);
+  }.observes("content")
+});
